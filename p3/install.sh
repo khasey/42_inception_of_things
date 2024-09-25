b318ee1bc89671e24edf0eac03a3f8651c0ba598
Khasey_Nine_Z <92310470+khasey@users.noreply.github.com>
2024-09-25 19:42:33 +0200

    Add files via upload

--- p3/install.sh
+++ p3/install.sh
@@ -43,8 +43,10 @@ curl -s https://raw.githubusercontent.com/rancher/k3d/main/install.sh | bash
 
 echo -e "${GREEN}[INFO]  Installation de kubectl ===================>>>>>>>>//////${RESET}"
 
-# Installation de kubectl
-curl -LO "https://dl.k8s.io/release/$(curl -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+# Télécharger une version spécifique de kubectl
+KUBECTL_VERSION="v1.28.2" # Remplacez par la version souhaitée
+
+curl -LO "https://dl.k8s.io/release/${KUBECTL_VERSION}/bin/linux/amd64/kubectl"
 chmod +x kubectl
 mv kubectl /usr/local/bin/
 
